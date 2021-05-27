7dd38430fb4505564ab2f3f8fec73d7d1523d148
KCMicheal <kcmicheal@users.noreply.github.com>
2021-05-27 16:55:02 +0100

    redirect done

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
    <div>
-     <h1>Homepage</h1>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempora recusandae, veritatis asperiores perspiciatis blanditiis eligendi, dolorum possimus distinctio quas, iusto similique impedit amet consequuntur at illo. Soluta, consequatur necessitatibus.</p>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempora recusandae, veritatis asperiores perspiciatis blanditiis eligendi, dolorum possimus distinctio quas, iusto similique impedit amet consequuntur at illo. Soluta, consequatur necessitatibus.</p>
+     <h1 className={styles.title}>Homepage</h1>
+     <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempora recusandae, veritatis asperiores perspiciatis blanditiis eligendi, dolorum possimus distinctio quas, iusto similique impedit amet consequuntur at illo. Soluta, consequatur necessitatibus.</p>
+     <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempora recusandae, veritatis asperiores perspiciatis blanditiis eligendi, dolorum possimus distinctio quas, iusto similique impedit amet consequuntur at illo. Soluta, consequatur necessitatibus.</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempora recusandae, veritatis asperiores perspiciatis blanditiis eligendi, dolorum possimus distinctio quas, iusto similique impedit amet consequuntur at illo. Soluta, consequatur necessitatibus.</p>
     <Link href="/ninjas">
-      <a>See Ninja listing</a>
+      <a className={styles.btn}>See Ninja listing</a>
     </Link>
    </div>
   )
